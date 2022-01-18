fc9ac2e65c70abd8b493a2864ffb91a5bc21391f
Yashwanth Mudili <yashwanthmudili@Yashwanths-MacBook-Pro.local>
2022-01-18 15:08:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let uploadButtonEl = document.getElementById("uploadButton");
+
+uploadButtonEl.addEventListener("click",function(){
+    let uploadBtn = document.createElement('input');
+    uploadBtn.type="file";
+    console.log('click')
+})
